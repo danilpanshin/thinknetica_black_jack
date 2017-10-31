7e332af93738a045a3408a87b42bc737e8cfd936
pavel-sazonov <wismyl@ya.ru>
2017-10-31 15:30:12 +0300

    add user move and user take card

--- main.rb
+++ main.rb
@@ -12,6 +12,7 @@ class Main
     show_cards
     puts user_score
     bet
+    user_move
   end
 
   private
@@ -47,6 +48,33 @@ class Main
   def bet
     @user.bet
     @dealer.bet
+  end
+
+  def user_move_menu
+    puts "Choose action:"
+    puts "miss - 1"
+    puts "add card - 2"
+    puts "open cards - 3"
+    gets.chomp.to_i
+  end
+
+  def user_take_card
+    @deck.give_out_card(@user)
+      show_cards
+      @user.score = 0
+      puts user_score
+  end
+
+  def user_move
+    case user_move_menu
+    when 1
+      dealer_move
+    when 2
+      user_take_card
+      dealer_move
+    when 3
+      open_cards
+    end
   end
 end
 
